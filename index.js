172ac322ae7d7e08d5dce6d04557763dd0976774
Newton School <newtonschool@Kartik.local>
2024-06-09 14:21:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const getTodos = () => {
+  
+};
+
+const getTodo = (id) => {
+  
+};
+
+const createTodo = (todo) => {
+ 
+};
+
+const updateTodo = async (id, updates) => {
+
+};
+
+const deleteTodo = async (id, updates) => {
+
+};
+
+module.exports = {
+  getTodos,
+  getTodo,
+  createTodo,
+  deleteTodo,
+  updateTodo,
+};
