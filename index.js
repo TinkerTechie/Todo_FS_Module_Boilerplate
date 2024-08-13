95e99eb130638c297dfe4726abda5ede21603e43
Newton School <newtonschool@Kartik.local>
2024-08-13 12:00:02 +0530

    boilerplate updated to test only sync operations

--- index.js
+++ index.js
@@ -8,25 +8,10 @@ const updateTodoSync = (id, updates) => {};
 
 const deleteTodoSync = (id) => {};
 
-const getTodos = () => {};
-
-const getTodo = (id) => {};
-
-const createTodo = (todo) => {};
-
-const updateTodo = async (id, updates) => {};
-
-const deleteTodo = async (id) => {};
-
 module.exports = {
   getTodosSync,
   getTodoSync,
   createTodoSync,
   updateTodoSync,
   deleteTodoSync,
-  getTodos,
-  getTodo,
-  createTodo,
-  deleteTodo,
-  updateTodo,
 };
